11170ab2a23a2a41ae22a888fa2f162241e158f0
lucasraphael02 <lucasraphael@aluno.ufrb.edu.br>
2025-08-13 18:09:30 -0300

    controllers de forma manual e automatica, extensao do vscode para verificar rotas

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('home')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('exemplo')
+  exemplo() {
+    return 'exemplo';
   }
 }
