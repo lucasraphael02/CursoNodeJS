deb961c7e4fc0740b9e04d7c8f82a34bc40c57d8
lucasraphael02 <lucasraphael@aluno.ufrb.edu.br>
2025-09-03 09:13:53 -0300

    Adicionando o Injectable para evitar erro de dependencias

--- src/common/interceptors/error-handling.interceptor.ts
+++ src/common/interceptors/error-handling.interceptor.ts
@@ -1,6 +1,12 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
 import { catchError, throwError } from 'rxjs';
 
+@Injectable()
 export class ErrorHandlingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler<any>) {
     return next.handle().pipe(
